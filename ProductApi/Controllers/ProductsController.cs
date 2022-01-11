88356c9506ab9889515d871da74b5c1c355391d0
Jonathan.Downs <Jonathan.Downs@VUHL-B4SQ353>
2022-01-11 10:14:05 -0600

    Add project files.

---
+++ ProductApi/Controllers/ProductsController.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ProductApi.Daos;
+using ProductApi.Models;
+
+namespace ProductApi.Controllers
+{
+    [ApiController]
+    [ApiVersion("1.0")]
+    [Route("{version:apiVersion}/[controller]")]
+    [Produces("application/json")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly ProductContext _context;
+
+        public ProductsController(ProductContext context)
+        {
+            _context = context;
+
+            if (_context.Products.Any()) return;
+
+            ProductSeed.InitData(context);
+        }
+
+        [HttpGet]
+        [Route("")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public ActionResult<IQueryable<Product>> GetProducts([FromQuery] string department)
+        {
+            var result = _context.Products as IQueryable<Product>;
+
+            if (!string.IsNullOrEmpty(department))
+            {
+                result = result.Where(p => p.Department.StartsWith(department, StringComparison.InvariantCultureIgnoreCase));
+            }
+
+            return Ok(result
+                .OrderBy(p => p.ProductNumber)
+                .Take(15));
+        }
+
+        [HttpGet]
+        [Route("{productNumber}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public ActionResult<IQueryable<Product>> GetProductsById([FromRoute] string productNumber)
+        {
+            var result = _context.Products as IQueryable<Product>;
+
+            if (!string.IsNullOrEmpty(productNumber))
+            {
+                result = result.Where(p => p.ProductNumber.Contains(productNumber));
+            }
+
+            return Ok(result
+                .OrderBy(p => p.ProductNumber)
+                .Take(15));
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public ActionResult<Product> PostProduct([FromBody] Product product)
+        {
+            try
+            {
+                _context.Products.Add(product);
+                _context.SaveChanges();
+
+                return new CreatedResult($"/products/{product.ProductNumber.ToLower()}", product);
+            }
+            catch (Exception e)
+            {
+                // Typically an error log is produced here
+                return ValidationProblem(e.Message);
+            }
+        }
+    }
+}
